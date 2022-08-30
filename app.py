5737c18094823941b22b5f24270e6cf152eaa5b5
Fahd <flaslam@gmail.com>
2022-08-30 13:00:45 +0100

    add base functionality for retrieving and displaying images from twitter API

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, jsonify, render_template
+import configparser
+import tweepy
+from flask_cors import CORS
+
+# Read configs
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+api_key = config['twitter']['api_key']
+api_key_secret = config['twitter']['api_key_secret']
+
+access_token = config['twitter']['access_token']
+access_token_secret = config['twitter']['access_token_secret']
+
+# Authenticate app with Twitter API
+auth = tweepy.OAuthHandler(api_key, api_key_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Create API instance
+api = tweepy.API(auth)
+
+# Initialise app
+app = Flask(__name__)
+
+# enable cors for dev
+CORS(app)
+
+# Get data from Twitter
+def get_favorites(username, last_id = None):
+  images = []
+  last_max_id = last_id
+  min_images = 30
+  count = 0
+  max_iterations = 1
+
+  while len(images) < min_images and count < max_iterations:
+    count+= 1
+    posts = api.get_favorites(screen_name=username, count=20, include_entities=True, tweet_mode="extended", max_id=last_max_id)
+    for post in posts:
+      last_max_id = post.id
+      try:
+        if 'media' in post.extended_entities:
+          for image in post.extended_entities['media']:
+            if (image['type']) == "photo":
+              images.append(image['media_url'])
+      except AttributeError:
+        print("No media on this post by " + post.user.screen_name)  
+  
+  data = {"images": images, "last_id": last_max_id}
+  return data
+
+# Routes
+@app.route("/<username>", methods=['GET'])
+@app.route("/<username>/<last_id>", methods=['GET'])
+def get(username, last_id = None):
+  data = get_favorites(username, last_id)
+  response = jsonify(data)
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  return response
+
+@app.route("/")
+def home():
+  return render_template("index.html")
+
+# Run server
+if __name__ == '__main__':
+  # app.run()
+  app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,125 @@
+const formElem = document.querySelector("form");
+formElem.addEventListener("submit", (e) => submitForm(e));
+
+const shuffleBtn = document.getElementById("shuffle");
+shuffleBtn.addEventListener("click", (e) => shuffleImages(e));
+
+const moreBtn = document.getElementById("more");
+moreBtn.addEventListener("click", (e) => loadMore(e));
+
+const increaseBtn = document.getElementById("increase");
+increaseBtn.addEventListener("click", (e) => changeSize(-1));
+
+const decreaseBtn = document.getElementById("decrease");
+decreaseBtn.addEventListener("click", (e) => changeSize(1));
+
+const imageHolder = document.getElementById("image-holder");
+
+let currColCount = 4;
+let user = null;
+let lastId = null;
+
+const changeSize = (amount) => {
+  if ((currColCount < 2 && amount < 0) || (currColCount > 6 && amount > 0))
+    return;
+
+  currColCount += amount;
+  imageHolder.style.columnCount = currColCount;
+};
+
+const submitForm = async (e) => {
+  e.preventDefault();
+
+  // Clear current images
+  imageHolder.innerHTML = "";
+
+  // Get username from form input field
+  const username = e.target[0].value;
+
+  try {
+    // Get data
+    const data = await getData(username);
+
+    // Store user for searching "more"
+    user = username;
+
+    // Process data to output images
+    processData(data);
+  } catch (err) {
+    alert(err);
+    console.log(err);
+  }
+};
+
+const processData = (data) => {
+  addImages(data.images);
+
+  // Store variables we need later.
+  lastId = data.last_id;
+};
+
+const addImages = (imgArr) => {
+  imgArr.forEach((element, index) => {
+    const imageContainer = document.createElement("div");
+    imageContainer.classList.add("image-container");
+
+    const img = document.createElement("img");
+    img.src = element;
+    // img.style.animationDelay = `${index / 16}s;`;
+
+    const deleteBtn = document.createElement("div");
+    deleteBtn.classList.add("delete-button");
+
+    // deleteBtn.textContent = "X";
+    const deleteBtnText = document.createElement("div");
+    // deleteBtnText.textContent = "X";
+    deleteBtn.appendChild(deleteBtnText);
+    // deleteBtn.style.position = "absolute";
+    // deleteBtn.style.padding = "1rem";
+    imageContainer.appendChild(deleteBtn);
+
+    imageContainer.appendChild(img);
+
+    const target = imageHolder.appendChild(imageContainer);
+
+    deleteBtn.addEventListener("click", (e) => target.remove());
+  });
+};
+
+const getData = async (username, id = null) => {
+  let url = `/${username}${id ? `/${id}` : ""}`;
+
+  try {
+    const response = await fetch(url, { method: "GET", mode: "cors" });
+    return response.json();
+  } catch (err) {
+    alert("Could not retrieve data.");
+    console.log(err);
+  }
+};
+
+const loadMore = async () => {
+  if (!lastId) {
+    alert("Search for a user before ");
+    return;
+  }
+  const data = await getData(user, lastId);
+  processData(data);
+};
+
+const shuffleImages = (e) => {
+  let imageArray = Array.prototype.slice.call(imageHolder.childNodes);
+  console.log(imageArray);
+  let newArr = shuffle(imageArray);
+  imageArray.innerHTML = "";
+  newArr.forEach((element) => imageHolder.appendChild(element));
+};
+
+// Fisher-Yates shuffle algorithm
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
